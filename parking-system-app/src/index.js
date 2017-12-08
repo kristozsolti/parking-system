107c22a4363a77d29377db2af2c79b4c5f715c7c
Kristó Zsolt <kristo.zsolti@gmail.com>
2017-12-08 11:26:09 +0200

    React-Bootstrap implemented to project, demo files deleted

--- parking-system-app/src/index.js
+++ parking-system-app/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import {Glyphicon} from 'react-bootstrap';
+
+const App = () => {
+    return(
+        <div>
+            <h1>Hello React <Glyphicon glyph="heart" />  </h1>
+        </div>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
