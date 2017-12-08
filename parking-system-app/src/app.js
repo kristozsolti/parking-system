592c2fc6fd2430bfde88838da5202612ae918e97
Kristó Zsolt <kristo.zsolti@gmail.com>
2017-12-08 12:18:39 +0200

    Separate App into an independent component.

---
+++ parking-system-app/src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Glyphicon} from 'react-bootstrap';
+
+const App = () => {
+    return(
+        <div>
+            <h1>Hello React <Glyphicon glyph="heart" />  </h1>
+        </div>
+    );
+};
+export default App;
--- parking-system-app/src/index.js
+++ parking-system-app/src/index.js
@@ -2,14 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import {Glyphicon} from 'react-bootstrap';
-
-const App = () => {
-    return(
-        <div>
-            <h1>Hello React <Glyphicon glyph="heart" />  </h1>
-        </div>
-    );
-}
+import App from './app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
