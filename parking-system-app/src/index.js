cd4f72d50f392ed063bf971001c7fc4e8836665d
Kristó Zsolt <kristo.zsolti@gmail.com>
2017-12-14 12:11:51 +0200

    Page title modified

--- parking-system-app/src/index.js
+++ parking-system-app/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './app';
+document.title = "Parking System";
 
 ReactDOM.render(<App />, document.getElementById('root'));
