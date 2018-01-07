3255cd46436a607bc713959b9238f4dab04ff238
Kristó Zsolt <kristo.zsolti@gmail.com>
2018-01-07 21:55:23 +0200

    getting things ready

--- parking-system-app/src/app.js
+++ parking-system-app/src/app.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Glyphicon} from 'react-bootstrap';
 
-const App = () => {
-    return(
-        <div>
-            <h1>Hello React <Glyphicon glyph="heart" />  </h1>
-        </div>
-    );
+class App extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Hello React <Glyphicon glyph="heart" />  </h1>
+            </div>
+        );
+    }
 };
 export default App;
